065d0d55b6cdf11f1db98f9358045b5e99b57cbd
johnnyGogo <johnny.liu@tarokosoftware.com>
2020-10-23 09:41:30 +0800

    Create a Blueprint
    https://flask.palletsprojects.com/en/1.1.x/tutorial/views/#create-a-blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 
-from . import db
+from . import db, auth
 
 def create_app(test_config=None):
     # create and configure the app
@@ -28,6 +28,8 @@ def create_app(test_config=None):
         pass
 
     db.init_app(app)
+
+    app.register_blueprint(auth.bp)
 
     # a simple page that says hello
     @app.route('/hello')
