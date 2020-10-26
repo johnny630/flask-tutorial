353393763cb5b43da5c30cef880c4c81da496216
johnnyliu <bakolama@gmail.com>
2020-10-26 23:37:43 +0800

    Blog delete
    https://flask.palletsprojects.com/en/1.1.x/tutorial/blog/#delete

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -90,5 +90,9 @@ def update(id):
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
-def delete():
-    pass
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
