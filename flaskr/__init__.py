d61c465d1faf3f7a2fad7f60be92ed6791e3061b
johnnyGogo <johnny.liu@tarokosoftware.com>
2020-10-26 08:58:47 +0800

    Blog Blueprint The Blueprint
    https://flask.palletsprojects.com/en/1.1.x/tutorial/blog/#blog-blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 
-from . import db, auth
+from . import db, auth, blog
 
 def create_app(test_config=None):
     # create and configure the app
@@ -30,6 +30,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     # a simple page that says hello
     @app.route('/hello')
