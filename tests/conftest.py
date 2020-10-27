5c0ae0519233366333c96d40fe4afafc11581370
johnnyGogo <johnny.liu@tarokosoftware.com>
2020-10-27 12:45:53 +0800

    Testing authentication
    https://flask.palletsprojects.com/en/1.1.x/tutorial/tests/#authentication

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,20 @@ from flaskr.db import get_db, init_db
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf-8')
 
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
@@ -33,3 +47,7 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
